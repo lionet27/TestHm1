b74aec6a719e8ebff47ee4525a1b678e07302d27
Soboleva Olga <osoboleva27@gmail.com>
2023-10-19 23:00:25 +0300

    Revert "add void PrintNotebooks" -task2
    This reverts commit ce3fdd907fa181c25f860e8e5014347b088a012d.

--- FilterNotebook.java
+++ FilterNotebook.java
@@ -1,15 +1,5 @@
-
-//     Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
-//     Создать множество ноутбуков.
-//     Написать метод, который будет запрашивать у пользователя критерий фильтрации и выведет ноутбуки, отвечающие фильтру. 
-// Критерии фильтрации можно хранить в Map. минимум 5 
-
-import java.util.ArrayList;
-// import java.util.List;
-import java.util.Scanner;
-
-
 public class FilterNotebook {
+
     public static void PrintNotebooks(ArrayList<Notebook> notebooks){
         for (int i=0; i<notebooks.size();i++) {
             System.out.println(notebooks.get(i).toString());
@@ -112,5 +102,7 @@ public class FilterNotebook {
         sc.close();
         return filterNotebooks;
     }
+
+>>>>>>> parent of ce3fdd9 (add void PrintNotebooks)
     
 }
